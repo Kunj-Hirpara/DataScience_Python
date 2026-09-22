6944ed8790d872ce884350a56765207e3802284f
Kunj Hirpara <kunjhirapara61@gmail.com>
2026-09-22 17:59:46 +0530

    Basic Programs

---
+++ d8.py
@@ -0,0 +1,47 @@
+# 1. Create menu-driven calculator program using a module. (add, subtract, multiply, divide)
+
+def add(a,b):
+    return a + b
+
+def sub(a,b):
+    return a - b
+
+def mul(a,b):
+    return a * b
+
+def div(a,b):
+    if b != 0:
+        return a / b
+    else:
+        print("Invalid for Zero")
+
+
+# 2. Calculates area and volume of shapes like Circle, Rectangle, Sphere, and Cube using
+# module.(two module separate module files for area and volume)
+
+
+
+# 3. Find the second largest number from a list.
+
+
+
+# 4. Remove duplicates items from list without using set.
+
+
+
+# 5. Write a Python program using a nested dictionary to store student records.
+
+
+
+# 6. Each student has a unique student ID, a name, and a dictionary of marks for three subjects:
+# Math, Science, and English.
+# Perform the following tasks:
+# 1. Store the details of at least three students using a nested dictionary format.
+# 2. Iterate through the dictionary and print each student’s name and their total marks.
+# 7. Find topper from above program.
+# 8. Each patient has a unique Patient ID, a name, and a dictionary of test results for three health
+# parameters: Blood Pressure, Blood Sugar, and Cholesterol Level.
+# 9. Perform the following tasks:
+# • Store the details of at least three patients using a nested dictionary format.
+# • Iterate through the dictionary and print each patient’s name and the total of their test values.
+# • Find the healthiest patient, assuming lower total test values indicate better health.
---
+++ d8_main.py
@@ -0,0 +1,31 @@
+import d8
+
+print("1. Add")
+print("2. Sub")
+print("3. Mul")
+print("4. div")
+
+choice = int(input("Enter your choice: "))
+if choice == 1:
+    num1 = int(input("Enter num1: "))
+    num2 = int(input("Enter num2: "))
+    res = d8.add(num1,num2)
+    print("Add: ", res)
+elif choice == 2:
+    num1 = int(input("Enter num1: "))
+    num2 = int(input("Enter num2: "))
+    res = d8.sub(num1,num2)
+    print("Sub: ", res)
+elif choice == 3:
+    num1 = int(input("Enter num1: "))
+    num2 = int(input("Enter num2: "))
+    res = d8.mul(num1,num2)
+    print("Mul: ", res)
+elif choice == 4:
+    num1 = int(input("Enter num1: "))
+    num2 = int(input("Enter num2: "))
+    res = d8.div(num1,num2)
+    if num2 != 0:
+        print("Div: ", res)
+else:
+    print("Invalid.")
